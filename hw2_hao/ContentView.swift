09021cc90b893f148b7f426f23ae20f340436121
YiChunWang <partypie3091@gmail.com>
2019-10-18 15:06:05 +0800

    Initial Commit

---
+++ hw2_hao/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hw2_hao
+//
+//  Created by blaire on 2019/10/18.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
